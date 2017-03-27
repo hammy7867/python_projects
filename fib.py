928d67cf86da83504ee42106110d150a2f31a282
Alexander Hamilton <hammy7867@gmail.com>
2017-03-27 10:53:06 -0600

    fixed spaces

--- fib.py
+++ fib.py
@@ -1,8 +1,8 @@
 def fib(n):
-        a = 0
-        b = 1
-        for i in xrange(n):
-                a,b = a + b, a
-                print a
+    a = 0
+    b = 1
+    for i in xrange(n):
+        a,b = a + b, a
+        print a
 
 fib(20)
