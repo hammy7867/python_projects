bf5d4fedf6f5e48c49c44172319a8f5de6b6f2b0
Alexander Hamilton <hammy7867@gmail.com>
2017-03-27 10:51:42 -0600

    Fixed spaces

--- singly_linked.py
+++ singly_linked.py
@@ -6,35 +6,35 @@ class Node():
         self.next = None
 
 class LinkedList():
-		def __init__(self):
-			self.head = None
+    def __init__(self):
+        self.head = None
 
-		def add_node(self, new_data):
-			new_node = Node(new_data)
-			new_node.next = self.head
-			self.head = new_node
+    def add_node(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
 
-		def print_list(self):
-			print self.head.data
-			temp = self.head.next
-			while (temp != None):
-				print temp.data
-				temp = temp.next
+    def print_list(self):
+        print self.head.data
+        temp = self.head.next
+        while (temp != None):
+	    print temp.data
+	    temp = temp.next
 
-		def reverse(self):
-			prev = self.head
-			self.head = self.head.next
-			prev.next = None
-			while (self.head.next is not None):
-				nxt = self.head.next
-				self.head.next = prev
-				prev = self.head
-				self.head = nxt
-			self.head.next = prev
+    def reverse(self):
+        prev = self.head
+        self.head = self.head.next
+        prev.next = None
+        while (self.head.next is not None):
+            nxt = self.head.next
+            self.head.next = prev
+            prev = self.head
+            self.head = nxt
+        self.head.next = prev
 
 llist = LinkedList()
 for x in xrange(11):
-        llist.add_node(x**2)
+    llist.add_node(x**2)
 print 'Current list order:'
 llist.print_list()
 llist.reverse()
